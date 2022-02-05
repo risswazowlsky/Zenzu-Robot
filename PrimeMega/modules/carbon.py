d10c963104a1887fae1185b293030a3b1409b784
risswazowlsky <85448152+risswazowlsky@users.noreply.github.com>
2022-02-05 19:49:13 +0700

    Update carbon.py

--- PrimeMega/modules/carbon.py
+++ PrimeMega/modules/carbon.py
@@ -27,8 +27,8 @@ async def carbon_func(_, message):
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_text(
-        f"""✨ **Hey I'm Prime Mega** 
-**Owner repo : [『TØNIC』](https://t.me/Bukan_guudlooking)**
+        f"""🤫 **Hey I'ts Zenzu Robot** 
+**Owner repo : [『Zenzu』](https://t.me/zenzuzu2)**
 **Python Version :** `{y()}`
 **Library Version :** `{o}`
 **Telethon Version :** `{s}`
@@ -38,8 +38,8 @@ async def repo(_, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Repo", url="https://github.com/Tonic990/PrimeMega"), 
-                    InlineKeyboardButton("Support", url="https://t.me/PrimeSupportGroup")
+                    InlineKeyboardButton("Groups", url="https://t.me/ZenzuRobotSupport"), 
+                    InlineKeyboardButton("Channel", url="https://t.me/Zenzu_shop")
                 ]
             ]
         ),
